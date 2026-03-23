930fabfb047a6e97b37852ceabea268d35a1ce0e
Alicja Przybylik <alaprzybylik@gmail.com>
2026-03-23 08:08:41 +0100

    changed inside loop (for neg in negatives) to vectorized solution for better performance

--- ns-skip-gram.py
+++ ns-skip-gram.py
@@ -3,7 +3,8 @@ from collections import Counter
 import re
 
 def sig(x):
-    return 1/(1+np.exp(-x))
+    return 1/(1+np.exp(-np.clip(x, -15, 15)))
+
 
 def generate_pairs(token_ids, window):
     l = len(token_ids)
@@ -22,7 +23,7 @@ def generate_pairs(token_ids, window):
 # --- TRAINING DATA PREPARATION --- #
 
 with open("t.txt", "r", encoding="utf-8") as file:
-    clean_text = re.sub(r'[^\w\s]', '', file.read()).lower()
+    clean_text = re.sub(r'[^\w\s]', ' ', file.read()).lower()
     
 tokens = clean_text.split()
 
@@ -33,7 +34,7 @@ id_to_word = {i: w for w, i in word_to_id.items()}
 token_ids = [word_to_id[w] for w in tokens]
 V = len(vocab)
 
-pairs = generate_pairs(token_ids=token_ids, window=3)
+pairs = generate_pairs(token_ids=token_ids, window=4)
 
 word_counter = Counter(token_ids)
 counts = np.array([word_counter[i] for i in range(V)])
@@ -45,14 +46,15 @@ noise = np.random.choice(V, size=noise_size, p=probabilities)
 # --- PARAMS INITIALIZATION --- #
 
 
-N = 50
-K = 5
+N = 100
+K = 10
 W1 = np.random.randn(V, N)*0.1
 W2 = np.random.randn(V, N)*0.1
 lr = 0.05
 prev = float('inf')
 ups = 0
 pointer = 0
+eps = 1e-12
 
 # --- CORE TRAINING LOOP --- #
 
@@ -60,34 +62,29 @@ for epoch in range(10):
     loss = 0
     
     for center, context in pairs:
+        
         c = W1[center]
         w = W2[context]
-        dc = np.dot(sig(np.dot(c, w))-1, w)
-        dw = np.dot(sig(np.dot(c, w))-1, c)
-        W2[context] -= lr*dw
 
-        x = np.dot(c, w)
-        loss -= np.log(sig(x))
-
-
+        if pointer+K >= noise_size:
+            pointer = 0
         negatives = noise[pointer : pointer+K]
+        pointer = (pointer+K)
+        NEG = W2[negatives]
 
-        if len(negatives) < K:
-            num_missing = K - len(negatives)
-            negatives = np.concatenate([negatives, noise[:num_missing]])
-
-        pointer = (pointer+K)%noise_size
-        
-        for neg in negatives:
-            w = W2[neg]
-            dc += np.dot(sig(np.dot(c, w)), w)
-            dw = np.dot(sig(np.dot(c, w)), c)
-            W2[neg] -= lr*dw
-
-            x = np.dot(c, w)
-            loss -= np.log(sig(-x))
+        dw = np.dot(sig(np.dot(c, w))-1, c)
+        sig_neg = sig(NEG @ c)
+        dNEG = np.outer(sig_neg, c)
+        dc = np.dot(sig(np.dot(c, w))-1, w) + sig_neg @ NEG
 
         W1[center] -= lr*dc
+        W2[context] -= lr*dw
+        
+        np.subtract.at(W2, negatives, lr * dNEG)
+
+        loss += -np.log(sig(np.dot(c, w)) + eps) - np.sum(np.log(1.0 - sig_neg + eps))
+    
+    loss /= len(pairs)
     if loss > prev:
         print("↑")
         end = 1
@@ -97,7 +94,7 @@ for epoch in range(10):
     prev = loss
     print("epoch", epoch, ":", loss)
     if ups == 2:
-        break
+        continue
 
 # --- END OF TRAINING LOOP --- #
 
@@ -120,8 +117,11 @@ while True:
 
         query_vec = W_normalized[word_id]
         similarities = np.dot(W_normalized, query_vec)
-        
         top_indices = np.argsort(similarities)[::-1][1:11] 
+
+        query_vec = W1[word_id]
+        distances = np.linalg.norm(W1 - query_vec, axis=1)
+        top_indices = np.argsort(distances)[1:11]
         
         for idx in top_indices:
             print(id_to_word[idx], similarities[idx])
